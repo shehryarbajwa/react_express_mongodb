f94232c77718a778956562fe334ca8401cdf41b2
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-01-17 03:33:09 -0500

    added build

---
+++ build/precache-manifest.b67e7d35a766065b49043abc61c1b8c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3bfe95167ff6d04b087760fe61bd529",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96cd07414d8c4d70a650",
+    "url": "/static/css/main.671a7501.chunk.css"
+  },
+  {
+    "revision": "2cc59f0b9beac08c2e45",
+    "url": "/static/js/2.9f4348fe.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.9f4348fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "96cd07414d8c4d70a650",
+    "url": "/static/js/main.57de3314.chunk.js"
+  },
+  {
+    "revision": "9dd0f9650715df0201d4",
+    "url": "/static/js/runtime-main.00f2dc29.js"
+  }
+]);

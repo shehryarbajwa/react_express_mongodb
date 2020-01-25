fcdf858dee6319ec504876980ee81e99e52c829e
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-01-25 17:47:49 -0500

    herokyu deploy

--- index.js
+++ index.js
@@ -4,93 +4,94 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const Note = require("./models/note");
-const {errorHandler, unknownEndpoint, requestLogger} = require("./notifications/error_handling.js")
+const {
+  errorHandler,
+  unknownEndpoint,
+  requestLogger
+} = require("./notifications/error_handling.js");
 
 const app = express();
 app.use(express.static("build"));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(requestLogger)
+app.use(requestLogger);
 
 //Error Handling
-
 
 app.get("/api", (request, response) => {
   response.send("<p>Shehryar Bajwa built this</p>");
 });
 
 app.get("/api/notes/:id", (request, response, next) => {
-  Note.findById(request.params.id).then(note => {
-    if (note){
-      response.json(note.toJSON());
-    }
-    else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => next(error))
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note.toJSON());
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error));
 });
 
 app.get("/api/notes", (request, response) => {
   Note.find({})
-  .then(notes => {
-    if(notes){
-      response.json(notes.map(note => note.toJSON()));
-    } else{
-      response.status(404).end();
-    }
-  })
-  .catch(error => {
-    console.log(error);
-    response.status(400).send({ error: 'malformatted request' })
-  })
+    .then(notes => {
+      if (notes) {
+        response.json(notes.map(note => note.toJSON()));
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      response.status(400).send({ error: "malformatted request" });
+    });
 });
 
-
-
-app.post('/api/notes', (request, response, next) => {
-  const body = request.body
+app.post("/api/notes", (request, response, next) => {
+  const body = request.body;
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
-  })
+    date: new Date()
+  });
 
   note
     .save()
     .then(savedNote => savedNote.toJSON())
     .then(savedAndFormattedNote => {
-      response.json(savedAndFormattedNote)
-    }) 
-    .catch(error => next(error)) 
-})
+      response.json(savedAndFormattedNote);
+    })
+    .catch(error => next(error));
+});
 
-app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body;
 
   const note = {
     content: body.content,
-    important: body.important,
-  }
+    important: body.important
+  };
 
   Note.findByIdAndUpdate(request.params.id, note)
     .then(updatedNote => {
-      response.json(updatedNote.toJSON())
+      response.json(updatedNote.toJSON());
     })
-    .catch(error => next(error))
-})
+    .catch(error => next(error));
+});
 
 app.delete("/api/notes/:id", (request, response, next) => {
   Note.findByIdAndRemove(request.params.id)
-  .then(result => {
-    if(result){
-      response.status(204).end();
-    } else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => next(error))
+    .then(result => {
+      if (result) {
+        response.status(204).end();
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error));
 });
 
 app.use(unknownEndpoint);

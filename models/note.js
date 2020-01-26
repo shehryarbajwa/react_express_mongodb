913ec0a9229ff1b0345b121ceb720cb09c129569
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-01-25 22:59:55 -0500

    reusable components refactored

--- models/note.js
+++ models/note.js
@@ -1,17 +1,4 @@
-require('dotenv').config({ path: 'ENV_FILENAME' });
-const mongoose = require("mongoose");
-const url = process.env.MONGODB_URI;
-
-console.log("connecting to", url);
-
-mongoose
-  .connect(url, { useNewUrlParser: true })
-  .then(result => {
-    console.log("connected to MongoDB");
-  })
-  .catch(error => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+const mongoose = require('mongoose')
 
 const noteSchema = new mongoose.Schema({
   content: {
@@ -24,18 +11,18 @@ const noteSchema = new mongoose.Schema({
     required: true
   },
   important: Boolean
-});
+})
 
 //Convert ID object of mongoose to a string.
 //Mongoose inbuilt transform method converts objects to string, or string to objects
 //Delete id and v for passing easier looking UI to front-end
 
-noteSchema.set("toJSON", {
+noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   }
-});
+})
 
-module.exports = mongoose.model("Note", noteSchema);
+module.exports = mongoose.model('Note', noteSchema)

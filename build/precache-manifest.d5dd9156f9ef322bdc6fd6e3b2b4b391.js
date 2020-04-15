6bbf090034c21b39c5dec620c5f35e037be66310
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-04-15 01:39:22 -0400

    updated react build

--- build/precache-manifest.d5dd9156f9ef322bdc6fd6e3b2b4b391.js
+++ build/precache-manifest.d5dd9156f9ef322bdc6fd6e3b2b4b391.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d1fba9588072702ceb829e1053d81927",
+    "revision": "58b3eef8ea08c3b5b86efb99cf68cc9e",
     "url": "/index.html"
   },
   {
-    "revision": "a5cf1e81ca109f8b9874",
+    "revision": "5d35554af730b78cff9b",
     "url": "/static/css/main.8bcffd3d.chunk.css"
   },
   {
-    "revision": "471287c27a40ce317526",
-    "url": "/static/js/2.50124fcd.chunk.js"
+    "revision": "622af871e26860234450",
+    "url": "/static/js/2.6cff2b77.chunk.js"
   },
   {
-    "revision": "a5cf1e81ca109f8b9874",
-    "url": "/static/js/main.085e903b.chunk.js"
+    "revision": "5d35554af730b78cff9b",
+    "url": "/static/js/main.34c1811e.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
